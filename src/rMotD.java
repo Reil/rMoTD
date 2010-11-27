8d9521213a66a313a319849e72572ed8d59eb213
Reil <f.israel@mail.utexas.edu>
2010-11-27 13:36:45 -0600

    Very basic framework: done!

--- src/rMotD.java
+++ src/rMotD.java
@@ -1,3 +1,18 @@
-public class rMotD {
-
+public class rMotD extends Plugin {
+	PropertiesFile Messages;
+	PluginListener listener = new rMotDListener();
+	public void initialize(){
+		Messages = new PropertiesFile("rMotD.properties");
+		etc.getLoader().addListener(PluginLoader.Hook.COMMAND, listener, this, PluginListener.Priority.MEDIUM);
+	}
+	
+	public void enable(){
+		
+	}
+	public void disable(){
+		Messages.save();
+	}
+	private class rMotDListener extends PluginListener {
+		
+	}
 }
