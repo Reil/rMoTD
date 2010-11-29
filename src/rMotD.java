3ba6d4d2310a9ddd56b88ffb4e5a780fdd359faf
unknown <fdssdsd@.(none)>
2010-11-28 22:36:45 -0600

    Shit works now!

--- src/rMotD.java
+++ src/rMotD.java
@@ -1,24 +1,40 @@
 import java.util.ArrayList;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class rMotD extends Plugin {
-	PropertiesFile Messages;
+	public PropertiesFile Messages;
 	PluginListener listener = new rMotDListener();
 	Logger log = Logger.getLogger("Minecraft");
 	
+	public rMotD () {
+		Messages = new PropertiesFile("rMotD.properties");
+	}
+	
 	public void initialize(){
-		etc.getLoader().addListener(PluginLoader.Hook.COMMAND, listener, this, PluginListener.Priority.MEDIUM);
+		etc.getLoader().addListener(PluginLoader.Hook.LOGIN, listener, this, PluginListener.Priority.MEDIUM);
 	}
 	public void enable(){
-		Messages = new PropertiesFile("rMotD.properties");
+		try {
+			Messages.load();
+		} catch (Exception e) {
+			log.log(Level.SEVERE, "[rMotD]: Exception while loading properties file.", e);
+		}
+		
 		log.info("[rMotD] Loaded!");
 		// etc.getInstance().addCommand("/newmessage", "Adds new message and stuff");
 	}
 	public void disable(){
-		Messages.save();
+		/* Messages.save(); */
 		log.info("[rMotD] Disabled!");
-	}
-	public Player [] sendToGroups (String [] sendToGroups, String message) {
+	} 
+	
+	/* Sends a the message string to each group named in sendToGroups */
+	public void sendToGroups (String [] sendToGroups, String message) {
+		String prompt = "Sending \"" + message + "\" to: ";
+		for (String Group:sendToGroups)
+			prompt = prompt + " " + Group;
+		
 		ArrayList <Player> sentTo = new ArrayList<Player>();
 		for (Player messageMe: etc.getServer().getPlayerList()){
 			boolean flag = false;
@@ -33,15 +49,15 @@ public class rMotD extends Plugin {
 				sentTo.add(messageMe);
 			}
 		}
-		return (Player[]) sentTo.toArray();
+		return;
 	}
-	
-	private class rMotDListener extends PluginListener {
+	 
+	public class rMotDListener extends PluginListener {
 		public void onLogin(Player triggerMessage){
 			String [] groupArray = triggerMessage.getGroups();
 			for (String groupName : groupArray){
 				if (Messages.keyExists(groupName)){
-					String sendToGroups_Message = Messages.getString(groupName,":");
+					String sendToGroups_Message = Messages.getString(groupName);
 					String [] Split =  sendToGroups_Message.split(":");
 					String [] sendToGroups = Split[0].split(",");
 					String message = Split[1];
--- src/rPropertiesFile.java
+++ src/rPropertiesFile.java
@@ -1,9 +1,17 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.logging.Logger;
 
 public class rPropertiesFile {
 	Hashtable<String,ArrayList<String>> Properties = new Hashtable<String,ArrayList<String>>();
-	
+	ArrayList<String> orders; 
+	String fileName;
+	Logger log = Logger.getLogger("Minecraft");
+
 	/**
      * Creates or opens a properties file using specified filename
      * 
@@ -36,37 +44,47 @@ public class rPropertiesFile {
 	int	getInt(java.lang.String key, int value){
 		return 0;
 	}
-	long	getLong(java.lang.String key) {
+	long getLong(java.lang.String key) {
 		return 0;
 	}
-	long	getLong(java.lang.String key, long value){
+	long getLong(java.lang.String key, long value){
 		return 0;
 	}
-	java.lang.String	getString(java.lang.String key) {
+	java.lang.String getString(java.lang.String key) {
 		return "Hey0!";
 	}
-	java.lang.String	getString(java.lang.String key, java.lang.String value) {
+	java.lang.String getString(java.lang.String key, java.lang.String value) {
 		return "Hey0!";
 	}
 	boolean	keyExists(java.lang.String key) {
 		return true;
 	}
-	void	load(){
+	void load() throws IOException {
+		/* Go through, line by line. 
+		 * If the line starts with # or !, then save the line in list
+		 * If the line has an assignment, put the name here. */
+		orders.clear();
+		BufferedReader reader;
+        reader = new BufferedReader(new FileReader(fileName));
+        String line;
+        while ((line = reader.readLine()) != null) {
+        	
+        }
 		
 	}
-	void	save(){
+	void save(){
 		
 	}
-	void	setBoolean(java.lang.String key, boolean value) {
+	void setBoolean(java.lang.String key, boolean value) {
 		
 	}
-	void	setInt(java.lang.String key, int value) {
+	void setInt(java.lang.String key, int value) {
 		
 	}
-	void	setLong(java.lang.String key, long value) {
+	void setLong(java.lang.String key, long value) {
 		
 	}
-	void	setString(java.lang.String key, java.lang.String value) {
+	void setString(java.lang.String key, java.lang.String value) {
 		
 	}
 }
